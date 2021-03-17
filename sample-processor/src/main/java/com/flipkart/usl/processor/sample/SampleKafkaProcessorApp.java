34064ef82b1f187c4faf509597f3ee4c91e2d7ae
Tanroop <tanroop.dhillon3@myntra.com>
2021-03-17 22:48:52 +0530

    Renamed

--- sample-processor/src/main/java/com/flipkart/usl/processor/sample/SampleKafkaProcessorApp.java
+++ sample-processor/src/main/java/com/flipkart/usl/processor/sample/SampleKafkaProcessorApp.java
@@ -2,7 +2,7 @@ package com.flipkart.usl.processor.sample;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import com.flipkart.gap.usl.core.config.ExternalKafkaConfigurationModule;
+import com.flipkart.gap.usl.core.config.KafkaIngestionConfigurationModule;
 import com.flipkart.gap.usl.core.helper.SparkHelper;
 import com.flipkart.gap.usl.core.metric.JmxReporterMetricRegistry;
 import com.flipkart.gap.usl.core.processor.ExternalKafkaPublisher;
@@ -24,7 +24,7 @@ public class SampleKafkaProcessorApp {
         // Initialise Document Registries if any.
         SparkHelper.bootstrap();
         SampleKafkaPublisherAppConfig sampleAppConfig = yamlMapper.readValue(new File(args[0]), SampleKafkaPublisherAppConfig.class);
-        Injector injector = Guice.createInjector(new ExternalKafkaConfigurationModule(sampleAppConfig.getCoreConfig()));
+        Injector injector = Guice.createInjector(new KafkaIngestionConfigurationModule(sampleAppConfig.getCoreConfig()));
         ExternalKafkaPublisher eventStreamProcessor = injector.getInstance(ExternalKafkaPublisher.class);
         log.info("EventStreamConsumer created");
         eventStreamProcessor.process();
--- sample-processor/src/main/java/com/flipkart/usl/processor/sample/SampleKafkaPublisherAppConfig.java
+++ sample-processor/src/main/java/com/flipkart/usl/processor/sample/SampleKafkaPublisherAppConfig.java
@@ -1,11 +1,11 @@
 package com.flipkart.usl.processor.sample;
 
-import com.flipkart.gap.usl.core.config.v2.ExternalKafkaApplicationConfiguration;
+import com.flipkart.gap.usl.core.config.v2.KafkaIngestionApplicationConfiguration;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class SampleKafkaPublisherAppConfig {
-    private ExternalKafkaApplicationConfiguration coreConfig;
+    private KafkaIngestionApplicationConfiguration coreConfig;
 }
